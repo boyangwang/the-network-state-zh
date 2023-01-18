c2ce0c9233874f93d63e33fc72a9366381fd4873
Boyang <wangboyang1991@gmail.com>
2023-01-18 22:00:44 +0800

    Update config.js

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,7 +1,7 @@
 const { description } = require('../../package')
 
 module.exports = {
-  base: '/the-network-state-zh/',
+  base: '/',
 
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
